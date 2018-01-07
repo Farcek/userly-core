7f2b0f3997eac704b091d677b04c4d6f993410ec
Farcek <farcek@gmail.com>
2018-01-07 23:23:27 +0800

    add findLockupUser user service

--- service/user/login.ts
+++ service/user/login.ts
@@ -8,28 +8,39 @@ export interface IParam {
     password: string
 }
 
-export async function findLoginUser(UserModel: IModel.IUser.IModel, appid: number, param: IParam) {
-
-    let pass = ICommon.md5(param.password.trim());
-
-    if (ICommon.isEmail(param.useridentity)) {
-        let email = param.useridentity;
+export async function findLockupUser(UserModel: IModel.IUser.IModel, appid: number, useridentity: string) {
+    if (ICommon.isEmail(useridentity)) {
+        let email = useridentity;
         var it = await IModel.IUser.findByEmail(UserModel, appid, email);
-        if (it && it.password === pass) {
+        if (it) {
             return it;
         }
-    } else if (ICommon.isPhone(param.useridentity)) {
-        let phone = param.useridentity;
+    } else if (ICommon.isPhone(useridentity)) {
+        let phone = useridentity;
         var it = await IModel.IUser.findByPhone(UserModel, appid, phone);
-        if (it && it.password === pass) {
+        if (it) {
             return it;
         }
     }
 
-    throw new errors.OtherError("not found user && not password mached")
+    throw new errors.OtherError("not found user")
 }
 
-export async function doLogin(user:IModel.IUser.IInstance){
+export async function findLoginUser(UserModel: IModel.IUser.IModel, appid: number, param: IParam) {
+
+    let pass = ICommon.md5(param.password.trim());
+
+    let user = await findLockupUser(UserModel, appid, param.useridentity);
+
+
+    if (user.password === pass) {
+        return user;
+    }
+
+    throw new errors.OtherError("not mached password")
+}
+
+export async function doLogin(user: IModel.IUser.IInstance) {
     user.login_at = new Date();
     user.beforelogin_at = user.login_at;
     await user.save();
