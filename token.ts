6e7fc94f99c828eae7b986a39e039f55d8be03de
Farcek <farcek@gmail.com>
2018-01-10 16:29:59 +0800

    fix tokens
     clear user token

--- token.ts
+++ token.ts
@@ -1,30 +1,30 @@
 import * as ICommon from './common';
 const jwt = require('jsonwebtoken');
-export class UserToken {
-    private static pass = 'jh32g4';
-    static gen(userid: string, appid: number) {
-        return ICommon.jwtEncryption({ u: userid, a: appid }, { pass: UserToken.pass, jwtid: 'user', expiresIn: '2h' });
-    }
-    static parse(token: string) {
-        var data = ICommon.jwtDecryption<{ u: string, a: number }>(token, { pass: UserToken.pass, jwtid: 'user' });
-        return {
-            userid: data.u, appid: data.a
-        }
-    }
-}
+// export class UserToken {
+//     private static pass = 'jh32g4';
+//     static gen(userid: string, appid: number) {
+//         return ICommon.jwtEncryption({ u: userid, a: appid }, { pass: UserToken.pass, jwtid: 'user', expiresIn: '2h' });
+//     }
+//     static parse(token: string) {
+//         var data = ICommon.jwtDecryption<{ u: string, a: number }>(token, { pass: UserToken.pass, jwtid: 'user' });
+//         return {
+//             userid: data.u, appid: data.a
+//         }
+//     }
+// }
 
-export interface IClientToken { 
+export interface IClientToken {
     appid: number
 }
 
 export class ClientToken {
 
     static gen(appid: number, pass: string): string {
-        return ICommon.jwtEncryption({ a : appid }, { pass, jwtid: 'client', expiresIn: '1h' });
+        return ICommon.jwtEncryption({ a: appid }, { pass, jwtid: 'client', expiresIn: '1h' });
     }
 
     static parse(token: string): IClientToken | null {
-        var decode = jwt.decode(token);        
+        var decode = jwt.decode(token);
         if (decode && decode.a && decode.jti == 'client') {
             return { appid: decode.a };
         }
@@ -44,19 +44,19 @@ export class ClientToken {
 
 export interface IConsoleUsertoken {
     userid: string
-    usertoken: string
+
 }
 
 export class ConsoleToken {
-    private static pass = 'asdfv234234af';
-    static gen(userid: string, usertoken: string): string {
-        return ICommon.jwtEncryption({ u: userid, t: usertoken }, { pass: ConsoleToken.pass, jwtid: 'userlyconsole', expiresIn: '1h' });
+
+    static gen(userid: string, pass: string): string {
+        return ICommon.jwtEncryption({ u: userid, t: '' }, { pass, jwtid: 'userlyconsole', expiresIn: '1h' });
     }
-    static parse(token: string): IConsoleUsertoken {
-        var data = ICommon.jwtDecryption<{ u: string, t: string }>(token, { pass: ConsoleToken.pass, jwtid: 'userlyconsole' });
+    static parse(token: string, pass: string): IConsoleUsertoken {
+        var data = ICommon.jwtDecryption<{ u: string, t: string }>(token, { pass, jwtid: 'userlyconsole' });
         //console.log('data', data)
         return {
-            userid: data.u, usertoken: data.t
+            userid: data.u
         }
     }
 }
